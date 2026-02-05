4cabb0c617d68c5dfb71c4f4eebf855b816edce1
AN-Fran <buzon03@xilae.com>
2026-02-05 16:48:24 +0100

    Update medusa-config.ts

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,8 +1,8 @@
-import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+import { loadEnv, defineConfig } from "@medusajs/framework/utils"
 
-loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+loadEnv(process.env.NODE_ENV || "development", process.cwd())
 
-module.exports = defineConfig({
+export default defineConfig({
   admin: {
     disable: true,
   },
@@ -12,12 +12,12 @@ module.exports = defineConfig({
     redisUrl: process.env.REDIS_URL,
 
     http: {
-      storeCors: process.env.STORE_CORS || "*",
-      adminCors: process.env.ADMIN_CORS || "*",
-      authCors: process.env.AUTH_CORS || "*",
+      storeCors: process.env.STORE_CORS || "https://shop.airsoftnation.eu",
+      adminCors: process.env.ADMIN_CORS || "https://admin.airsoftnation.eu",
+      authCors: process.env.AUTH_CORS || "https://shop.airsoftnation.eu",
 
-      jwtSecret: process.env.JWT_SECRET || "supersecret",
-      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
-  }
+      jwtSecret: process.env.JWT_SECRET!,
+      cookieSecret: process.env.COOKIE_SECRET!,
+    },
+  },
 })
