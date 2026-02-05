6c0bf49655d400c5caa4bb09d8f66d583ea81b9f
AN-Fran <buzon03@xilae.com>
2026-02-05 19:03:15 +0100

    Update medusa-config.ts

--- medusa-config.ts
+++ medusa-config.ts
@@ -3,19 +3,41 @@ import { loadEnv, defineConfig } from "@medusajs/framework/utils"
 loadEnv(process.env.NODE_ENV || "development", process.cwd())
 
 export default defineConfig({
+  /**
+   * Admin UI
+   * Se expone en admin.airsoftnation.eu
+   */
   admin: {
     disable: false,
   },
 
   projectConfig: {
+    /**
+     * Infraestructura
+     */
     databaseUrl: process.env.DATABASE_URL,
     redisUrl: process.env.REDIS_URL,
 
+    /**
+     * HTTP / CORS
+     */
     http: {
-      storeCors: process.env.STORE_CORS || "https://shop.airsoftnation.eu",
-      adminCors: process.env.ADMIN_CORS || "https://admin.airsoftnation.eu",
-      authCors: process.env.AUTH_CORS || "https://shop.airsoftnation.eu",
+      // Storefront (Next.js)
+      storeCors:
+        process.env.STORE_CORS || "https://shop.airsoftnation.eu",
 
+      // Admin UI
+      adminCors:
+        process.env.ADMIN_CORS || "https://admin.airsoftnation.eu",
+
+      // Auth (login, sessions)
+      authCors:
+        process.env.AUTH_CORS ||
+        "https://shop.airsoftnation.eu,https://admin.airsoftnation.eu",
+
+      /**
+       * Seguridad
+       */
       jwtSecret: process.env.JWT_SECRET!,
       cookieSecret: process.env.COOKIE_SECRET!,
     },
